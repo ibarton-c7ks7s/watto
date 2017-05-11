3a89b291a8eae5b7433a9895fd85d8c0b35ace23
Vinnie Garcia <vinnie@coev.co>
2017-05-10 20:07:58 -0700

    feat(init): initial commit w/some dependencies
    First commit. Used create-react-app to start up.
    
    None

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <div className="App-header-inner">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h2>Watto's Space Emporium</h2>
+          </div>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
